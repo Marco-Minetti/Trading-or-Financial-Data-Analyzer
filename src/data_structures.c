6468d4aacdbf8b4b20ba7f2c842ccf071d0a2638
Jeremy <jeremyorozco38@gmail.com>
2025-04-12 16:51:54 -0500

    Changes to h and c hopefully it works

--- src/data_structures.c
+++ src/data_structures.c
@@ -4,60 +4,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 int numberOfNodes = 0;
-
-typedef struct {
-    unsigned char date[20]: 4;
-    double price, open, high, low, volume, change;
-}Data;
-
-typedef struct Node{
-    Data d;
-    struct Node* next;
-}Node;
 
 //this function will take all the vars and allocate space for it then be passed to create the node
 //the constant char allows you pass a string literal safely to the function
-void createStruct(const char date, double price, double open, double high, double low, double volume, double change){
-    Data *d = malloc(sizeof(Data));
-    strncpy(d->date, date, sizeof(d->date) - 1);
-    d->date[sizeof(d->date) - 1] = '\0';
-    d->price = price;
-    d->open = open;
-    d->high = high;
-    d->low = low;
-    d->volume = volume;
-    d->change = change;
-    nodesCreate(d);
+void createStruct(const char* date, double price, double open, double high, double low, double volume, double change){
+   Data *d = malloc(sizeof(Data));
+   strncpy(d->date, date, sizeof(d->date) - 1);
+   d->date[sizeof(d->date) - 1] = '\0';
+   d->price = price;
+   d->open = open;
+   d->high = high;
+   d->low = low;
+   d->volume = volume;
+   d->change = change;
+   nodesCreate(d);
 }
+
 
 //this function will create the node, hold the data and will be initialized to NULL and then be passed to nodesCombined to be linked together
 void nodesCreate(Data *d){
-    Node *n = malloc(sizeof(Node));
-    n->d = *d;
-    n->next = NULL;
-    nodesCombined(n);
+   Node *n = malloc(sizeof(Node));
+   n->d = *d;
+   n->next = NULL;
+   nodesCombined(n);
 }
 
+
 Node* nodesCombined(Node *n){ //the node from nodesCreate will be passed here and connected to each other
-    static Node *head = NULL; //will retain its state when the function is done, and it wont be reinitialized on each call
-    Node *temp = n;
-   
-    while(temp != NULL){
-        Node *ref = temp->next; //will always be set to null due to temp->next being always being NULL
-        temp->next = head;
-        head = temp;
-        temp = ref; //will refer to ref and temp will be set to NULL
-        numberOfNodes++;
-    }
-    return head;
+   static Node *head = NULL; //will retain its state when the function is done, and it wont be reinitialized on each call
+   Node *temp = n;
+  
+   while(temp != NULL){
+       Node *ref = temp->next; //will always be set to null due to temp->next being always being NULL
+       temp->next = head;
+       head = temp;
+       temp = ref; //will refer to ref and temp will be set to NULL
+       numberOfNodes++;
+   }
+   return head;
 }
 
 void clearingMemory(Node *head){
-    Node *temp = head;
-    while(temp != NULL){
-        head = head->next;
-        free(temp);
-        temp = head;
-    }
+   Node *temp = head;
+   while(temp != NULL){
+       head = head->next;
+       free(temp);
+       temp = head;
+   }
 }
--- src/data_structures.h
+++ src/data_structures.h
@@ -2,23 +2,22 @@
 #define DATA_STRUCTURES_H
 
 extern int numberOfNodes; //global variable used to keep track of nodes possibly
-extern omp_set_num_threads(2);
 
 typedef struct {
-    unsigned char date[20]: 4;
-    double price, open, high, low, volume, change;
+   char date[20];
+   double price, open, high, low, volume, change;
 }Data;
 
 typedef struct Node{
-    Data d;
-    struct Node* next;
+   Data d;
+   struct Node* next;
 }Node;
 
 void nodesCreate(Data *d);
 
 Node* nodesCombined(Node *n);
 
-void createStruct(char const date, double price, double open, double high, double low, double volume, double change);
+void createStruct(char const *date, double price, double open, double high, double low, double volume, double change);
 
 void clearingMemory(Node *head);
 
